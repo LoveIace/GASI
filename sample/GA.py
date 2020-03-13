dd119566aa089eb21a5faf98d8558f2cd604c0f9
thombedej <mandrake321@gmail.com>
2020-03-13 18:59:53 +0100

    added different GA individuals

--- sample/GA.py
+++ sample/GA.py
@@ -1,26 +1,40 @@
 import array
 import random
 
-# class Individual:
-#     def __init__(self, genotype_len):
-#         self.fitness = 0
-#         self.genotype = array.array('B', [0]) * genotype_len
-
 def shuffle(list):
     for i in range(len(list)-1, 0, -1): 
-        j = random.randint(0, i + 1)  
+        j = random.randint(0, i) 
         list[i], list[j] = list[j], list[i] 
 
 class Individual_TSP:
-    def __init__(self, number_of_cities):
+    def __init__(self, genotype_len):
         self.fitness = None
         self.genotype = []
-        for i in range(number_of_cities):
+        for i in range(genotype_len):
             self.genotype.append(i+1)
         shuffle(self.genotype)
 
-def main(size):
-    test = Individual_TSP(size)
-    print(test.genotype)
+class Individual_SLF:
+    def __init__(self, genotype_len):
+        self.fitness = None
+        self.genotype = array.array('B', [0]) * genotype_len
+        for i in range(genotype_len):
+            if random.random() < 0.5:
+                self.genotype[i] = 1
 
-main(30)
+class Individual_HEF:
+    def __init__(self, genotype_len, start, stop):
+        self.fitness = None
+        self.genotype = []
+        for i in range(genotype_len):
+            self.genotype.append(random.uniform(start,stop))
+
+
+def main(pop_size, size):
+    population = []
+    for i in range(pop_size):
+        population.append(Individual_HEF(size, 0, 100))
+    for i in population:
+        print(i.genotype)
+
+main(30,10)
